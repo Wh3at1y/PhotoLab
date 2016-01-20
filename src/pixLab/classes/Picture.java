23f18574ffa699c3ef3109fb12a597e96282a079
Sam Montoya <smon7958@740s-pl-118.local>
2016-01-20 13:46:35 -0700

    Added green, red, blue changes

--- src/pixLab/classes/Picture.java
+++ src/pixLab/classes/Picture.java
@@ -99,6 +99,37 @@ public class Picture extends SimplePicture
     }
   }
   
+  public void zeroRed()
+  {
+	  Pixel[][] redPixels = this.getPixels2D();
+	  for(int row = 0; row < redPixels.length; row++)
+		  {
+			  for(int col = 0; col < redPixels[0].length; col++)
+				  {
+					  Pixel currentPixel = redPixels[row][col];
+					  currentPixel.setRed(0);
+					  
+					  redPixels[row][col].setRed(0);
+				  }
+		  }
+  }
+  
+  public void zeroGreen()
+	  {
+		  Pixel[][] greenPixels = this.getPixels2D();
+		  for(int row = 0; row < greenPixels.length; row++)
+			  {
+				  for(int col = 0; col < greenPixels[0].length; col++)
+					  {
+						  Pixel currentPixel = greenPixels[row][col];
+						  currentPixel.setGreen(0);
+						  
+						  greenPixels[row][col].setRed(0);
+					  }
+			  }
+	  }
+  
+  
   /** Method that mirrors the picture around a 
     * vertical mirror in the center of the picture
     * from left to right */
@@ -226,6 +257,8 @@ public class Picture extends SimplePicture
   {
     Picture beach = new Picture("beach.jpg");
     beach.explore();
+    beach.zeroGreen();
+    beach.zeroRed();
     beach.zeroBlue();
     beach.explore();
   }
